4c315e8958fe97027a6fcefad3060e14e8652dde
Raja Sekhar <50092598+krajasekhar015@users.noreply.github.com>
2019-05-01 16:48:11 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,7 @@
+class A {
+ public static void main(String args[])
+{
+
+     System.out.println("Hello World"+args[0]+args[1]);
+ }
+}
---
+++ Subtraction.java
@@ -0,0 +1,11 @@
+class Subtraction
+{
+public static void main(String args[])
+ {
+ int a=2;
+ int b=3;
+ int c;
+ c=a-b;
+ System.out.println("Subtraction ="+c);
+ }
+}
