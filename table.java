f24a252b2d1056a36119ab9899ba07c2e07c3dc7
Raja Sekhar <50092598+krajasekhar015@users.noreply.github.com>
2019-05-06 10:52:52 +0530

    Add files via upload

---
+++ table.java
@@ -0,0 +1,13 @@
+class MulTable
+{
+	public static void main(String args[])
+	{
+		int i=Integer.parseInt(args[0]);
+		int j=1;
+		while(j<=10)
+	{
+		System.out.println( i + "*" + j + "=" + i*j);
+		j++;
+	}
+	}
+}
